b95b51f3bcc9d487adae6aaa7bf5d813a0002da2
Archit-d300 <architdeshpande.study@gmail.com>
2026-02-10 17:05:16 +0530

    Connected to DB

--- journalApp/src/main/java/net/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/journalApp/controller/JournalEntryController.java
@@ -1,41 +1,54 @@
 package net.journalApp.controller;
 
 import net.journalApp.entity.JournalEntry;
+import net.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private Map<String,JournalEntry> journalEntries=new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
 
-// Method inside Controller class should be public so that they can be accessed and invoked by spring framework.
+    // Method inside Controller class should be public so that they can be accessed and invoked by spring framework.
     @GetMapping
     public List<JournalEntry> getAll(){
-        return new ArrayList<>(journalEntries.values());
+        return journalEntryService.getAll();
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry newEntry){
-        journalEntries.put(newEntry.getId(),newEntry);
-        return true;
+    public JournalEntry createEntry(@RequestBody JournalEntry newEntry){
+        newEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(newEntry);
+        return newEntry;
     }
 
     @GetMapping("/id/{Id}")
-    public JournalEntry getJournalEntryById(@PathVariable String Id){
-        return journalEntries.get(Id);
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId Id){
+        return journalEntryService.findById(Id).orElse(null);
     }
 
     @DeleteMapping("/id/{Id}")
-    public JournalEntry deleteEntryById(@PathVariable String Id){
-        return journalEntries.remove(Id);
+    public boolean deleteEntryById(@PathVariable ObjectId Id){
+        journalEntryService.deleteById(Id);
+        return true;
     }
 
     @PutMapping("/id/{Id}")
-    public JournalEntry updateEntryById(@PathVariable String Id,@RequestBody JournalEntry newEntry){
-        return journalEntries.put(Id,newEntry);
+    public JournalEntry updateEntryById(@PathVariable ObjectId Id,@RequestBody JournalEntry newEntry){
+        JournalEntry old = journalEntryService.findById(Id).orElse(null);
+        if(old != null){
+            old.setTitle(newEntry.getTitle() != null && newEntry.getTitle().equals("") ? old.getTitle() : newEntry.getTitle());
+            old.setContent(newEntry.getContent() != null && newEntry.getContent().equals("") ? old.getContent() : newEntry.getContent());
+        }
+        journalEntryService.saveEntry(old);
+        return old;
     }
 
 }
--- journalApp/src/main/java/net/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/journalApp/entity/JournalEntry.java
@@ -1,13 +1,33 @@
 package net.journalApp.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 // POJO class (Plane Old Java Entry)
+@Document(collection = "journal_entries")
 public class JournalEntry {
-    private String Id;
+    @Id
+    private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
 
-    public String getId() {
-        return Id;
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+
+
+    public ObjectId getId() {
+        return id;
     }
 
     public String getContent() {
@@ -18,8 +38,8 @@ public class JournalEntry {
         this.content = content;
     }
 
-    public void setId(String id) {
-        Id = id;
+    public void setId(ObjectId id) {
+        this.id = id;
     }
 
     public String getTitle() {
