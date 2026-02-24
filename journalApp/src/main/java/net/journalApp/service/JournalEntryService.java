a1ebfd43771a504788e9ca4dfdc3dedbe21f5ada
Archit-d300 <architdeshpande.study@gmail.com>
2026-02-24 17:17:23 +0530

    Added Transactions

--- journalApp/src/main/java/net/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import net.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +21,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user=userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user=userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        }catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("Error occurred while saving the entry");
+        }
+
     }
 
     public void saveEntry(JournalEntry journalEntry){
