350ba6e0f12b324a033332cb7e4220302cbd4321
Archit-d300 <architdeshpande.study@gmail.com>
2026-02-27 15:23:34 +0530

    Added Security to User

--- journalApp/src/main/java/net/journalApp/service/UserService.java
+++ journalApp/src/main/java/net/journalApp/service/UserService.java
@@ -5,8 +5,12 @@ import net.journalApp.entity.User;
 import net.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +19,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void saveEntry(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("User"));
         userRepository.save(user);
+
+    }
+
+    public void saveNewEntry(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("User"));
+        userRepository.save(user);
+
     }
 
     public List<User> getAll(){
